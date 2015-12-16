1a309455ca32edc0cef9504a1d9f8448c7880ddd
juhlmadsen1982@gmail.com <juhlmadsen1982@gmail.com>
2015-12-16 14:57:40 +0100

    Vores Trio forsætter stadig !
    små rettelser i view. skønhedsfejl
    
    
    Hilsen Fluttershy , Rainbow dash & Pinkie pie:)

--- semesterSeedSP/web/app/components/filters.js
+++ semesterSeedSP/web/app/components/filters.js
@@ -7,4 +7,17 @@ angular.module('myApp.filters', []).
     return function(input) {
       return input ? '\u2713' : '\u2718';
     };
-  });
+  })
+          .filter('minToHours',function (min) {
+        var result;
+        if (min < 60) {
+            result = min + "m";
+        }
+        else if (min % 60 == 0) {
+            result = (min - min % 60) / 60 + "h";
+        }
+        else {
+            result = ((min - min % 60) / 60 + "h" + " " + min % 60 + "m");
+        }
+        return result;
+    });
--- semesterSeedSP/web/app/view2/view2.js
+++ semesterSeedSP/web/app/view2/view2.js
@@ -3,27 +3,39 @@
 angular.module('myApp.view2', ['ngRoute'])
 
         .config(['$routeProvider', function ($routeProvider) {
-            $routeProvider.when('/view2', {
-              templateUrl: 'app/view2/view2.html',
-              controller: 'View2Ctrl'
-            });
-          }])
+                $routeProvider.when('/view2', {
+                    templateUrl: 'app/view2/view2.html',
+                    controller: 'View2Ctrl'
+                });
+            }])
 
         .controller('View2Ctrl', function ($http, $scope, userFactory) {
-          $scope.MSG = "";
-          $scope.loadImg = true;
-          var userName = userFactory.getUsername();
-          $http({
-            method: 'GET',
-            url: 'api/momondo/getreservation/'+ userName
-          }).then(function successCallback(res) {
-            $scope.loadImg = false;
-            $scope.data = res.data;
-             
-           
-          }, function errorCallback(res) {
-            $scope.loadImg = false;
-            $scope.MSG = "Your reservations were not found";
-          });
+            $scope.MSG = "";
+            $scope.loadImg = true;
+            var userName = userFactory.getUsername();
+            $http({
+                method: 'GET',
+                url: 'api/momondo/getreservation/' + userName
+            }).then(function successCallback(res) {
+                $scope.loadImg = false;
+                $scope.data = res.data;
 
+            }, function errorCallback(res) {
+                $scope.loadImg = false;
+                $scope.MSG = "Your reservations were not found";
+            });
+            
+            $scope.minToHours = function (min) {
+                var result;
+                if (min < 60) {
+                    result = min + "m";
+                }
+                else if (min % 60 == 0) {
+                    result = (min - min % 60) / 60 + "h";
+                }
+                else {
+                    result = ((min - min % 60) / 60 + "h" + " " + min % 60 + "m");
+                }
+                return result;
+            };
         });
